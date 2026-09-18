753bd5278fbcea1cde7e2fd07a1dd15d8bfc8156
HeavenCodesDev <heevenCaalaa11@gmail.com>
2026-09-18 06:57:39 -0500

    version 3

---
+++ Features/default-operator.js
@@ -0,0 +1,2 @@
+true || console.log('this is example of default operator');
+5/'NaN' || console.log('this is second example of default-operator ');
---
+++ Features/guardian-operator.js
@@ -0,0 +1,2 @@
+'this is true' && console.log('this is example of guardian operator');
+0 && console.log('this is second example of guardian operator');
